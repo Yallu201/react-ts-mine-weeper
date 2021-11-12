f99203a8f37b164590596130bb831ea85e39b6f2
yallu201 <dlruddlf07@gmail.com>
2021-11-12 23:13:06 +0900

    Add callback onClickReset to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import "./App.css";
 
 function App() {
   const [mineCount, setMineCount] = useState(10);
   const [timeSpent, setTimeSpent] = useState(0);
   const [stopwatch, setStopwatch] = useState<NodeJS.Timeout | null>(null);
+  const onClickReset = useCallback(() => {
+    stopwatch && clearInterval(stopwatch);
+    setTimeSpent(0);
+  }, [stopwatch]);
   return (
     <div className="container">
       <div className="header-wrap">
+        <input type="text" value={mineCount} />
+        <button onClick={onClickReset}>reset</button>
+        <input type="text" value={timeSpent} />
       </div>
       <div className="cell-area-wrap">
       </div>
