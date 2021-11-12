668b2958f79fbbacfa3d8204951d5169fb137120
yallu201 <dlruddlf07@gmail.com>
2021-11-12 23:07:35 +0900

    Add state 'mineCount', 'timeSpent', 'stopwatch' to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [mineCount, setMineCount] = useState(10);
+  const [timeSpent, setTimeSpent] = useState(0);
+  const [stopwatch, setStopwatch] = useState<NodeJS.Timeout | null>(null);
   return (
     <div className="container">
       <div className="header-wrap">
-
       </div>
       <div className="cell-area-wrap">
-        
       </div>
     </div>
   );
