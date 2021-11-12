f7d802ab78b8c5de4ec0a15c34abd920ad10defa
yallu201 <dlruddlf07@gmail.com>
2021-11-12 09:07:46 +0900

    Create Cell.ts

---
+++ src/store/Cell.ts
@@ -0,0 +1,48 @@
+import { makeObservable, observable } from "mobx";
+
+type CellIndex = number | null;
+export class Cell {
+  __row: CellIndex = null;
+  __column: CellIndex = null;
+  __isOpened: boolean = false;
+  __isMine: boolean = false;
+  __mineCount: number = 0;
+
+  constructor(row: number, column: number) {
+    this.__row = row;
+    this.__column = column;
+
+    makeObservable(this, {
+      __row: observable,
+      __column: observable,
+      __isOpened: observable,
+      __isMine: observable,
+      __mineCount: observable,
+    });
+  }
+
+  get row(): CellIndex {
+    return this.__row;
+  }
+  get column(): CellIndex {
+    return this.__column;
+  }
+  get isOpened(): boolean {
+    return this.__isOpened;
+  }
+  set isOpened(input: boolean) {
+    this.__isOpened = input;
+  }
+  get isMine(): boolean {
+    return this.__isMine;
+  }
+  set isMine(input: boolean) {
+    this.__isMine = input;
+  }
+  get mineCount(): number {
+    return this.__mineCount;
+  }
+  set mineCount(count: number) {
+    this.__mineCount = count;
+  }
+}
