697db3d4c22ff88859bdffdf6f71e9222da18ea3
yallu201 <dlruddlf07@gmail.com>
2021-11-15 21:10:33 +0900

    Add click count, size(row, column) and setMine function to CellBoard.ts

--- src/store/CellBoard.ts
+++ src/store/CellBoard.ts
@@ -1,17 +1,42 @@
-import { makeObservable, observable } from "mobx";
+import { action, makeObservable, observable } from "mobx";
+import { Cell } from ".";
 import CellRow from "./CellRow";
 
 export default class CellBoard {
+  __clickCount: number;
+  __rowCount: number;
+  __columnCount: number;
   __cellRows: CellRow[];
 
-  constructor(cellRows: CellRow[]) {
-    this.__cellRows = cellRows;
+  constructor() {
+    this.__clickCount = 0;
+    this.__rowCount = 9;
+    this.__columnCount = 9;
+    this.__cellRows = [];
+
+    for (let i = 0; i < this.__rowCount; i++) {
+      const row = new CellRow(i, []);
+      for (let j = 0; j < this.__columnCount; j++) {
+        const cell = new Cell(i, j);
+        row.push(cell);
+      }
+      this.__cellRows.push(row);
+    }
 
     makeObservable(this, {
       __cellRows: observable,
+      __rowCount: observable,
+      __columnCount: observable,
+      setMine: action,
     });
   }
 
+  get clickCount(): number {
+    return this.__clickCount;
+  }
+  set clickCount(count: number) {
+    this.__clickCount = count;
+  }
   get cellRows(): CellRow[] {
     return this.__cellRows;
   }
@@ -20,5 +45,47 @@ export default class CellBoard {
   }
   push(row: CellRow) {
     this.__cellRows.push(row);
+  }
+  setMine(row: number, col: number) {
+    const isTopRow = row === 0;
+    const isBottomRow = row === this.__rowCount - 1;
+    const isLeftColumn = col === 0;
+    const isRightColumn = col === this.__columnCount - 1;
+
+    this.cellRows[row].cells[col].isMine = true;
+
+    if (!isTopRow) {
+      // ■■■
+      // □□□
+      // □□□
+      this.cellRows[row - 1].cells[col].mineCount += 1;
+      if (!isLeftColumn) {
+        this.cellRows[row - 1].cells[col - 1].mineCount += 1;
+      }
+      if (!isRightColumn) {
+        this.cellRows[row - 1].cells[col + 1].mineCount += 1;
+      }
+    }
+    if (!isBottomRow) {
+      // □□□
+      // □□□
+      // ■■■
+      this.cellRows[row + 1].cells[col].mineCount += 1;
+      if (!isLeftColumn) {
+        this.cellRows[row + 1].cells[col - 1].mineCount += 1;
+      }
+      if (!isRightColumn) {
+        this.cellRows[row + 1].cells[col + 1].mineCount += 1;
+      }
+    }
+    // □□□
+    // ■□■
+    // □□□
+    if (!isLeftColumn) {
+      this.cellRows[row].cells[col - 1].mineCount += 1;
+    }
+    if (!isRightColumn) {
+      this.cellRows[row].cells[col + 1].mineCount += 1;
+    }
   }
 }
