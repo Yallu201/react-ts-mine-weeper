d2fec0f1b120adf3f67e7e08ef81b4955b6cfb3c
yallu201 <dlruddlf07@gmail.com>
2021-11-15 21:06:32 +0900

    Add onClick with setMine props to Cell.tsx

--- src/components/Cell.tsx
+++ src/components/Cell.tsx
@@ -1,11 +1,22 @@
+import { observer } from "mobx-react";
+import { useCallback } from "react";
 import { Cell } from "../store";
-type CellProps = { cell: Cell };
-const CellComponent = ({ cell }: CellProps) => {
+type CellProps = { cell: Cell; setMine: () => void };
+const CellComponent = observer(({ cell, setMine }: CellProps) => {
+  const onClick = useCallback(() => {
+    setMine();
+    cell.isOpened = true;
+  }, [setMine]);
   return (
-    <button className="cell">
+    <button
+      className={`cell ${cell.isMine ? "mine" : ""} ${
+        cell.isOpened ? "open" : ""
+      }`}
+      onClick={onClick}
+    >
       {cell.row}, {cell.column}
     </button>
   );
-};
+});
 
 export default CellComponent;
