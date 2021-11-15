ed7d7e1d8a86919a14463b860cbd247e9c58e83e
yallu201 <dlruddlf07@gmail.com>
2021-11-15 22:15:15 +0900

    Apply observer to Header.tsx for using useStore hook

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,19 @@
+import { observer } from "mobx-react";
+import { useCallback } from "react";
 import { useGameStore } from "../store/Game";
 
-const Header = () => {
+const Header = observer(() => {
   const store = useGameStore();
+  const onReset = useCallback(() => {
+    store.reset();
+  }, []);
   return (
     <div className="header-wrap">
       <input type="text" value={store.mineCount} />
-      <button onClick={store.reset}>reset</button>
+      <button onClick={onReset}>reset</button>
       <input type="text" value={store.seconds} />
     </div>
   );
-};
+});
 
 export default Header;
