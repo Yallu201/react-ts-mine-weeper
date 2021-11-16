d7b6f8146bdf428be498769ac765263a134f459f
yallu201 <dlruddlf07@gmail.com>
2021-11-16 22:00:51 +0900

    Apply BoardStore to Board component

--- src/components/Cell.tsx
+++ src/components/Cell.tsx
@@ -1,28 +1,38 @@
 import { observer } from "mobx-react";
 import { useCallback, useMemo } from "react";
 import { CellStore } from "../store";
+import { useBoardStore } from "../store/Board";
 import { useGameStore } from "../store/Game";
 type CellProps = { cell: CellStore; setMine: () => void };
 const CellComponent = observer(({ cell, setMine }: CellProps) => {
   const gameStore = useGameStore();
-
+  const boardStore = useBoardStore();
   const onClick = useCallback(() => {
+    if (gameStore.isGameOver) {
+      return;
+    }
+    cell.open();
     if (cell.isMine) {
       // 지뢰인 경우, 게임 정지 및 모든 지뢰 표시
+      boardStore.openMines();
+      gameStore.gameOver();
+      return;
     }
     setMine();
     gameStore.start();
-    cell.isOpened = true;
-  }, [setMine, cell, gameStore]);
+  }, [setMine, cell.isMine, cell.isOpened, gameStore.isGameOver, boardStore]);
 
   const content = useMemo(() => {
     const { mineCount, isMine } = cell;
     return isMine ? null : mineCount > 0 ? mineCount : null;
   }, [cell.mineCount, cell.isMine]);
+
   const className = useMemo(() => {
     const open = cell.isOpened ? (cell.isMine ? " mine open" : " open") : "";
-    return `cell${open}`;
-  }, [cell.isMine, cell.isOpened]);
+    const gameOver = gameStore.isGameOver ? " red" : "";
+    return `cell${open}${gameOver}`;
+  }, [cell.isMine, cell.isOpened, gameStore.isGameOver]);
+
   return (
     <button className={className} onClick={onClick}>
       {content}
