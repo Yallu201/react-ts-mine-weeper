b5ca116c08e5194be53ade935a3fa65943c4f249
yallu201 <dlruddlf07@gmail.com>
2021-11-16 21:20:57 +0900

    Apply useMemo to Cell.tsx

--- src/components/Cell.tsx
+++ src/components/Cell.tsx
@@ -1,8 +1,8 @@
 import { observer } from "mobx-react";
-import { useCallback } from "react";
+import { useCallback, useMemo } from "react";
 import { CellStore } from "../store";
 import { useGameStore } from "../store/Game";
-  type CellProps = { cell: CellStore; setMine: () => void };
+type CellProps = { cell: CellStore; setMine: () => void };
 const CellComponent = observer(({ cell, setMine }: CellProps) => {
   const gameStore = useGameStore();
 
@@ -10,15 +10,20 @@ const CellComponent = observer(({ cell, setMine }: CellProps) => {
     setMine();
     gameStore.start();
     cell.isOpened = true;
-  }, [setMine,cell,gameStore]);
+  }, [setMine, cell, gameStore]);
+
+  const content = useMemo(() => {
+    const { mineCount } = cell;
+    return mineCount > 0 ? mineCount : null;
+  }, [cell.mineCount]);
+  const className = useMemo(() => {
+    const mine = cell.isMine ? " mine" : "";
+    const open = cell.isOpened ? " open" : "";
+    return `cell${mine}${open}`;
+  }, [cell.isMine, cell.isOpened]);
   return (
-    <button
-      className={`cell ${cell.isMine ? "mine" : ""} ${
-        cell.isOpened ? "open" : ""
-      }`}
-      onClick={onClick}
-    >
-      {cell.row}, {cell.column}
+    <button className={className} onClick={onClick}>
+      {content}
     </button>
   );
 });
