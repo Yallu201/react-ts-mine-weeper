79a669a3f3d3a4b19d87282cfb91e4624cb2ba1d
yallu201 <dlruddlf07@gmail.com>
2021-11-16 21:29:26 +0900

    Update don't show mineCount of mine cell

--- src/components/Cell.tsx
+++ src/components/Cell.tsx
@@ -7,19 +7,21 @@ const CellComponent = observer(({ cell, setMine }: CellProps) => {
   const gameStore = useGameStore();
 
   const onClick = useCallback(() => {
+    if (cell.isMine) {
+      // 지뢰인 경우, 게임 정지 및 모든 지뢰 표시
+    }
     setMine();
     gameStore.start();
     cell.isOpened = true;
   }, [setMine, cell, gameStore]);
 
   const content = useMemo(() => {
-    const { mineCount } = cell;
-    return mineCount > 0 ? mineCount : null;
-  }, [cell.mineCount]);
+    const { mineCount, isMine } = cell;
+    return isMine ? null : mineCount > 0 ? mineCount : null;
+  }, [cell.mineCount, cell.isMine]);
   const className = useMemo(() => {
-    const mine = cell.isMine ? " mine" : "";
-    const open = cell.isOpened ? " open" : "";
-    return `cell${mine}${open}`;
+    const open = cell.isOpened ? (cell.isMine ? " mine open" : " open") : "";
+    return `cell${open}`;
   }, [cell.isMine, cell.isOpened]);
   return (
     <button className={className} onClick={onClick}>
