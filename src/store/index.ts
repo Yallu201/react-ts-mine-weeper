d2d81bab559178777384199c284e0f90bc7fc7e0
이경일 <leeki19@koreaap.com>
2021-11-16 10:27:48 +0900

    Add Game to store/index

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,3 +1,4 @@
 export { default as CellStore } from "./Cell";
 export { default as RowStore } from "./Row";
 export { default as BoardStore } from "./Board";
+export { default as GameStore } from "./Game";
