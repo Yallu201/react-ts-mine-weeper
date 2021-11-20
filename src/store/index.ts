1e131f58c3616a691bdda2d981c4114d92b81fdc
yallu201 <dlruddlf07@gmail.com>
2021-11-20 09:23:46 +0900

    Updates

--- src/store/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as CellStore } from "./Cell";
-export { default as RowStore } from "./Row";
-export { default as BoardStore } from "./Board";
-export { default as GameStore } from "./Game";
