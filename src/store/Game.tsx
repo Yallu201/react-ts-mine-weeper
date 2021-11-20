2543a460c3f4c4372655ae43f433f0599e71cdff
yallu201 <dlruddlf07@gmail.com>
2021-11-20 09:07:36 +0900

    Add checkCount, leftMineCount to game store

--- src/store/Game.tsx
+++ src/store/Game.tsx
@@ -1,9 +1,10 @@
-import { action, makeObservable, observable } from "mobx";
+import { action, computed, makeObservable, observable } from "mobx";
 import { createContext, FC, useContext } from "react";
 const ONE_SECOND = 1000;
 export default class GameStore {
   __seconds: number = 0;
   __mineCount: number = 10;
+  __checkCount: number = 0;
   __interval: NodeJS.Timeout | null = null;
   __isGameOver: boolean = false;
   __isGameStart: boolean = false;
@@ -11,10 +12,13 @@ export default class GameStore {
     makeObservable(this, {
       __seconds: observable,
       __mineCount: observable,
+      __checkCount: observable,
+      leftMineCount: computed,
       addSecond: action,
       start: action,
       reset: action,
       gameOver: action,
+      addCheckCount: action,
     });
   }
 
@@ -30,6 +34,10 @@ export default class GameStore {
   get isGameStart() {
     return this.__isGameStart;
   }
+
+  get leftMineCount() {
+    return Math.max(0, this.__mineCount - this.__checkCount);
+  }
   addSecond() {
     this.__seconds += 1;
   }
@@ -42,6 +50,7 @@ export default class GameStore {
   reset() {
     this.__seconds = 0;
     this.__mineCount = 10;
+    this.__checkCount = 0;
     this.__isGameStart = false;
     this.__isGameOver = false;
     if (!this.__interval) return;
@@ -54,6 +63,10 @@ export default class GameStore {
     if (!this.__interval) return;
     clearInterval(this.__interval);
     this.__interval = null;
+  }
+
+  addCheckCount(input: number) {
+    this.__checkCount += input;
   }
 }
 
